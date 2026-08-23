014b7d86fbd9c640bec2991587c0fa1ed71382d9
RamzypeeTech <okerekeprince111@gmail.com>
2026-08-23 17:28:02 +0100

    Fix CSRF trusted origins for Render mobile access

--- school_management/settings.py
+++ school_management/settings.py
@@ -29,6 +29,12 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://learn-pro.onrender.com',
+    'https://*.onrender.com',
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
